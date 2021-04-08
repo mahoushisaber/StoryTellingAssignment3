e4464d7ea2971bce55d539cb6b2caea693dabc07
John Dong <mahoushisaber@gmail.com>
2021-04-07 21:14:09 -0700

    Added Sound manager and Added rigid body to the items

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+	public Sound[] sounds;
+
+	void Awake()
+	{
+		foreach (Sound s in sounds)
+		{
+			s.source = gameObject.AddComponent<AudioSource>();
+			s.source.clip = s.clip;
+			s.source.loop = s.loop;
+			s.source.playOnAwake = s.playOnAwake;
+		}
+	}
+	void Start()
+    {
+		Play("BGMusic");
+
+/*		if (SceneManager.GetActiveScene().name == "GamePlay_1")
+		{
+			FindObjectOfType<AudioManager>().Play("Theme1");
+		}
+		if (SceneManager.GetActiveScene().name == "GamePlay_2")
+		{
+			FindObjectOfType<AudioManager>().Play("Theme3");
+		}
+
+		if (SceneManager.GetActiveScene().name == "GamePlay_3")
+		{
+			FindObjectOfType<AudioManager>().Play("Theme5");
+		}*/
+	}
+
+	public void Play(string sound)
+	{
+		Sound s = Array.Find(sounds, item => item.name == sound);
+		if (s == null)
+		{
+			Debug.LogWarning("Sound: " + name + " not found!");
+			return;
+		}
+
+		s.source.volume = s.volume * (1f + UnityEngine.Random.Range(-s.volumeVariance / 2f, s.volumeVariance / 2f));
+		s.source.pitch = s.pitch * (1f + UnityEngine.Random.Range(-s.pitchVariance / 2f, s.pitchVariance / 2f));
+
+		s.source.Play();
+	}
+
+	public void Stop(string sound)
+	{
+		Sound s = Array.Find(sounds, item => item.name == sound);
+		if (s == null)
+		{
+			Debug.LogWarning("Sound: " + name + " not found!");
+			return;
+		}
+
+		s.source.volume = s.volume * (1f + UnityEngine.Random.Range(-s.volumeVariance / 2f, s.volumeVariance / 2f));
+		s.source.pitch = s.pitch * (1f + UnityEngine.Random.Range(-s.pitchVariance / 2f, s.pitchVariance / 2f));
+
+		s.source.Stop();
+	}
+
+
+}
