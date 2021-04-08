a9feb8e477f23861b5d9fba446ab0b3b9add7cf1
AugeyT <71095950+AugeyT@users.noreply.github.com>
2021-04-07 22:02:02 -0700

    Added support for cursor change and prep to send signal to object waiting for investigate activation
    - So cursor will change shape based on hot zones
    - When in hot zone and the mouse is pressed it the framework is in place to send a signal to a monitoring object that can act on the investigate click.

--- Assets/Scripts/Priest.cs
+++ Assets/Scripts/Priest.cs
@@ -8,32 +8,106 @@ public class Priest : MonoBehaviour
 {
     private Rigidbody body;
     public Camera cam;
+    public float rayHitDistance;
+    public Canvas pointCursor;
 
     private const float movementSpeed = 5f;
     private const float rotationSpeed = 200f;
 
     private float curXRotation;
 
+    private RaycastHit rayHit;
+    private Ray ray;
+
+    [SerializeField]
+    private int investigate;
+    private int hitLive;
+    private int delayClearTimer;
+
+    private GameObject spriteOFF;
+    private GameObject spriteON;
+
     void Start()
     {
         body = GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Locked;
+        if (rayHitDistance < 5.0f)
+        {
+            rayHitDistance = 5.0f;
+        }
+        investigate = 0;
+        hitLive = 0;
+        delayClearTimer = 0;
+
+        spriteOFF = pointCursor.GetComponent<Transform>().Find("Crosshair").gameObject;
+        spriteON = pointCursor.GetComponent<Transform>().Find("Eye").gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // taken from https://www.youtube.com/watch?v=EANtTI6BCxk
-        if (Input.GetMouseButtonDown(0))
-        {
-            RaycastHit rayHit;
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        int lastHitLive = hitLive;
 
-            if (Physics.Raycast(ray, out rayHit, 10))
+        ray = cam.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out rayHit, rayHitDistance))
+        {
+            if (rayHit.transform)
             {
-                if (rayHit.transform)
+                switch (rayHit.transform.gameObject.name)
+                    {
+                    case "WoodenChest_Hit_1":
+                        hitLive = 1;
+                        break;
+
+                    case "Bible_Hit_2":
+                        hitLive = 2;
+                        break;
+
+                    case "Monk_Robes_Hit_3":
+                        hitLive = 3;
+                        break;
+
+                    default:
+                        hitLive = 0;
+                        break;
+                }
+
+                if (lastHitLive != hitLive)
                 {
-                    print(rayHit.transform.gameObject.name);
+                    if (pointCursor)
+                    {
+                        if (hitLive == 0)
+                        {
+                            spriteON.SetActive(false);
+                            spriteOFF.SetActive(true);
+                        }
+                        else
+                        {
+                            spriteON.SetActive(true);
+                            spriteOFF.SetActive(false);
+                        }
+                    }
+                    print(rayHit.transform.gameObject.name + " -- " + hitLive);
+                }
+
+                if (hitLive != 0 && Input.GetMouseButtonDown(0))
+                {
+                    investigate = hitLive;
+                    delayClearTimer = 0;
+                }
+                else
+                {
+                    // get rid of this after debugging and replace the investigate with proper message dispatch method to the handler!
+                    if (delayClearTimer >= 200)
+                    {
+                        delayClearTimer = 0;
+                        investigate = 0;
+                    }
+                    else
+                    {
+                        delayClearTimer += 1;
+                    }
                 }
             }
         }
