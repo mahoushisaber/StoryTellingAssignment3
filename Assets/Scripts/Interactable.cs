212e1aa73b68f98cff51c91156e17f9aa9dc3e9f
Thomas Bui <thomasbui198@gmail.com>
2021-04-11 11:40:22 -0700

    Added Interactable ScriptableObject

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Interactable")]
+public class Interactable : ScriptableObject
+{
+    public Sprite image;
+    public string text;
+}
