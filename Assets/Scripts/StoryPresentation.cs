93a71658c4d2558390102b0a3ab045a282f5d037
AugeyT <71095950+AugeyT@users.noreply.github.com>
2021-04-15 21:59:15 -0700

    Story has all been inserted. And added the Final question and summary

--- Assets/Scripts/StoryPresentation.cs
+++ Assets/Scripts/StoryPresentation.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class StoryPresentation : MonoBehaviour
 {
     private GameObject m_SelectPopUp;
+    private GameObject m_SelectEndingPopUp;
     private GameObject m_StoryPopup;
     private StoryPopUpMgr m_StoryPopUpMgr;
     private Item m_InteractableItem;
+    private GameObject m_CurrSelectPopup;
 
     public void Open(Item interactableItem)
     {
         // Get the Find SelectPopup object so we can change the GameObject active state
         m_SelectPopUp = this.GetComponent<Transform>().Find("SelectPopup").gameObject;
+        m_SelectEndingPopUp = this.GetComponent<Transform>().Find("SelectEndingPopup").gameObject;
 
         // We want to get script but because its parent is inactive we can't find it directly.
         // To compensate for this we can find its parent and then dig out the component
@@ -24,13 +27,21 @@ public class StoryPresentation : MonoBehaviour
 
         // All done so show the activate the game object and as the hieracy is setup this
         // will show over the last window and is what we want.
-        m_SelectPopUp.SetActive(true);
+        if (interactableItem.itemID < 9)
+        {
+            m_CurrSelectPopup = m_SelectPopUp;
+        }
+        else 
+        {
+            m_CurrSelectPopup = m_SelectEndingPopUp;
+        }
+        m_CurrSelectPopup.SetActive(true);
     }
 
     public void CloseSelectPopup()
     {
         //  On close click we just want to deactivate the popup
-        m_SelectPopUp.SetActive(false);
+        m_CurrSelectPopup.SetActive(false);
     }
 
     public void OnStoryItemClick(int StoryItemID)
